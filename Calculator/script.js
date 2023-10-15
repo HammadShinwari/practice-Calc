e277a02f4d9413e15cd1613eb36eb0a924962dab
HammadShinwari33 <hammadshinwari377@gmail.com>
2023-10-15 19:20:11 +0500

    Practice-Calc

---
+++ Calculator/script.js
@@ -0,0 +1,21 @@
+// ========================== Calculator ===========================
+
+let result = document.getElementById("result");
+
+function display(number){
+    result.value += number;
+};
+
+function calculate(){
+    let finalNumber = result.value;
+    let finalResult = eval(finalNumber);
+    result.value = finalResult;
+};
+
+function clrs(){
+    result.value = "";
+}
+
+function dle(){
+     result.value = result.value.slice(0,-1);
+}
